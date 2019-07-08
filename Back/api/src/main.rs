85599a91ae167c6fce70a0c795a7947c04813702
itrofimow <i.trofimow@yandex.ru>
2019-07-08 18:39:58 +0300

    postgres pool, ok-ish dockerfile (change runtime!)

--- Back/api/src/main.rs
+++ Back/api/src/main.rs
@@ -1,5 +1,5 @@
 use actix_web::{error::ErrorInternalServerError, web, App, Error, HttpServer};
-use futures::future::{lazy, ok, Future};
+use futures::future::{lazy, ok, err, Future};
 use futures::stream::Stream;
 use std::sync::Arc;
 use tokio::runtime::current_thread::Runtime;
@@ -10,57 +10,39 @@ use bb8::Pool;
 use bb8_postgres::PostgresConnectionManager;
 
 struct PekaRepository {
-    pool: Pool<PostgresConnectionManager<NoTls>>,
-    connection_string: &'static str,
+    pool: Pool<PostgresConnectionManager<NoTls>>
 }
 
 impl PekaRepository {
-    fn new(pool: Pool<PostgresConnectionManager<NoTls>>, connection_string: &'static str) -> Self {
+    fn new(pool: Pool<PostgresConnectionManager<NoTls>>) -> Self {
         PekaRepository {
-            pool,
-            connection_string,
+            pool
         }
     }
 
-    fn get_client(&self) -> impl Future<Item = Client, Error = DbError> {
-        Box::new(
-            tokio_postgres::connect(self.connection_string, NoTls)
-                .map(|(client, connection)| {
-                    let connection = connection.map_err(|e| {
-                        eprintln!("db connection error! {}", e);
-                    });
-                    actix::spawn(connection);
-
-                    client
-                })
-                .map_err(|e| {
-                    eprintln!("fatal db connection error {}", e);
-                    e
-                }),
-        )
-    }
-
-    fn test(&self) -> impl Future<Item = String, Error = DbError> {
-        self.get_client()
-            .and_then(|mut client| client.simple_query("select peka from peka;").collect())
-            .then(|db_res| {
-                let res = db_res.unwrap();
-                let str = match &res[0] {
-                    Row(row) => match row.get(0).take() {
-                        None => panic!(),
-                        Some(val) => val,
+    fn get_current_peka(&self) -> impl Future<Item = String, Error = DbError> {
+        self.pool.run(|mut connection: Client| {
+            connection.simple_query("select peka from peka;").collect().then(move |r| {
+                match r {
+                    Ok(res) => match &res[0] {
+                        Row(row) => match row.get(0).take() {
+                            None => panic!(),
+                            Some(val) => ok((String::from(val), connection))
+                        },
+                        _ => panic!()
                     },
-                    _ => panic!(),
-                };
-                ok::<String, DbError>(String::from(str))
+                    Err(e) => err((e, connection))
+                }
             })
+        })
+        .map_err(|_| panic!())
     }
 }
 
 fn get_current_peka(
     repo: web::Data<Arc<PekaRepository>>,
 ) -> Box<Future<Item = String, Error = Error>> {
-    Box::new(repo.test().map_err(|e| {
+    Box::new(repo.get_current_peka().map_err(|e| {
         println!("error! {}", e);
         ErrorInternalServerError(e)
     }))
@@ -78,7 +60,7 @@ fn main() {
             .map_err(|e| panic!(e))
     })).unwrap();
         
-    let peka_repo = Arc::new(PekaRepository::new(pool, "host=localhost port=26257 user=root"));
+    let peka_repo = Arc::new(PekaRepository::new(pool));
     let server = HttpServer::new(move || {
         App::new()
             .data(peka_repo.clone())
